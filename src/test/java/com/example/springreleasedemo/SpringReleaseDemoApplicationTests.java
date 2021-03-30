4e09e0a5ec17b3b26b37d1162a18a003fa1efac3
KushYadav <kushyadavnitd@gmail.com>
2021-03-30 17:29:50 +0530

    Initial Commit

---
+++ src/test/java/com/example/springreleasedemo/SpringReleaseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springreleasedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReleaseDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
